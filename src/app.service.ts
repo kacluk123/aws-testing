8f29856fb0299cde5433eae93b25c0b3f741de5c
kacluk123 <sakuy124@gmail.com>
2020-08-16 16:45:04 +0200

    dd

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+require('dotenv').config();
 const message = process.env.MESSAGE;
 
 @Injectable()
 export class AppService {
   getHello(): string {
+  console.log(process.env.MESSAGE)
     return `Hello ${message}`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv'
+
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
